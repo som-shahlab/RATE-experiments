3081341defa372b3363578a5e012ca3cf92aec0d
Steve Yadlowsky <yadlowsky@google.com>
2022-02-10 23:09:31 +0000

    Add shell script

---
+++ experiments/section_6_digital_marketing/run_experiment.sh
@@ -0,0 +1,3 @@
+echo "Please download the unbiased dataset at https://ailab.criteo.com/criteo-uplift-prediction-dataset/ to the ./data/ subdirectory if you have not already."
+Rscript criteo_analysis.R
+Rscript criteo_plot_results.R
